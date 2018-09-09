5595a489bac0b7dfc1c4aa17506aab54398fa002
Jock Dalby <jockdalby86@gmail.com>
2018-09-09 18:36:22 +1200

    clean up un-needed console.logs

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,7 +6,7 @@ const notes = require('./notes.js')
 const argv = yargs.argv;
 
 // NOTE: this is an array of all the command line arguments passed in when starting the app
-console.log('e.g. node app.js list',  '===> process.argv', process.argv);
+// console.log('e.g. node app.js list',  '===> process.argv', process.argv);
 /*
 [ '/usr/local/bin/node', // Refers to node command
   '/Users/jock/Desktop/node-sandbox/app.js', // refers to app.js
@@ -17,13 +15,12 @@ console.log('e.g. node app.js list',  '===> process.argv', process.argv);
 
  // **YARGS MAKES THE PROCESS OF PARSING COMMAND LINE ARGUMENTS MUCH MUCH EASIER**
  // This is argv we can access when using 3rd party module, yargs
- console.log('e.g. node app.js list --title=secrets',  '===> argv', argv);
+//  console.log('e.g. node app.js list --title=secrets',  '===> argv', argv);
 /*
  * { _: [ 'list' ], title: 'secrets', '$0': 'app.js' }
  */
 
 var command = argv._[0];
-console.log('Command: ', command);
 
 if (command === 'add') {
   const note = notes.addNote(argv.title, argv.body);
